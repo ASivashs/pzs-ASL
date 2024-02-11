ade5c7f3cd214f987860d82909ea4e4324fecc66
Anton Sivashka <anton.sivashko@gmail.com>
2024-02-11 17:24:39 +0300

    Test

--- pzs1_filex5.sh
+++ pzs1_filex5.sh
@@ -10,7 +10,7 @@ for dir in pzs11 pzs12 pzs13 pzs14; do
   cd $dir
   for file in file*5; do
     echo "Running $file at iit11"
-    su iit11 -c "bash $file &" 2>/dev/null
+    su iit11 -c "./$file &" 2>/dev/null
     echo "PID: $!"
     pid=$!
     for user in iit11 iit12 iit21 iit22 iit3 root; do
