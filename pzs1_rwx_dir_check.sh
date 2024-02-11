c4037d5fea087c8909d279931719f6c94079902d
Anton Sivashka <anton.sivashko@gmail.com>
2024-02-11 16:39:58 +0300

    Updated pzs1_rwx_dir_check.sh

--- pzs1_rwx_dir_check.sh
+++ pzs1_rwx_dir_check.sh
@@ -8,6 +8,7 @@ for dir in pzs11 pzs12 pzs13 pzs14 pzs15; do
   cd $dir
   for user in iit11 iit12 iit21 iit22 iit3 root; do
     echo "User $user:"
+
     # Проверить, можно ли прочитать содержимое папки
     su $user -c "ls" 2>/dev/null
     if [ $? -eq 0 ]; then
@@ -15,6 +16,7 @@ for dir in pzs11 pzs12 pzs13 pzs14 pzs15; do
     else
       echo "Cannot read $dir"
     fi
+
     # Проверить, можно ли создать новый файл в папке
     su $user -c "touch test" 2>/dev/null
     if [ $? -eq 0 ]; then
@@ -24,15 +26,15 @@ for dir in pzs11 pzs12 pzs13 pzs14 pzs15; do
     else
       echo "Cannot create new file in $dir"
     fi
+
     # Проверить, можно ли удалить каждый из существующих файлов
-    for file in file*; do
-      su $user -c "rm $file" 2>/dev/null
-      if [ $? -eq 0 ]; then
-        echo "Can delete $file"
-      else
-        echo "Cannot delete $file"
-      fi
-    done
+    su $user -c "rm file11" 2>/dev/null
+    if [ $? -eq 0 ]; then
+    echo "Can delete file"
+    else
+    echo "Cannot delete file"
+    fi
+
     echo "-----------------"
   done
   cd ..
